3629a4720626a32f2cbac1be9db86ad7dec13ac0
John Mcintosh <john.mcintosh@mutualmobile.com>
2013-01-30 14:11:20 -0600

    Initial Commit

---
+++ SlidingPanels/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SlidingPanels
+//
+//  Created by John Mcintosh  on 1/30/13.
+//  Copyright (c) 2013 John Mcintosh . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
